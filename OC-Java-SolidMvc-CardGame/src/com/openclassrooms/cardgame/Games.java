9eae57bd9178332308eb43f60421bca9eb29f497
lorraine <lorraine@LAPTOP-HT6C87G7>
2021-04-16 11:12:01 +0200

    GameEvaluator interface

--- OC-Java-SolidMvc-CardGame/src/com/openclassrooms/cardgame/Games.java
+++ OC-Java-SolidMvc-CardGame/src/com/openclassrooms/cardgame/Games.java
@@ -1,14 +1,15 @@
 package com.openclassrooms.cardgame;
 
 import com.openclassrooms.cardgame.controller.GameController;
-import com.openclassrooms.cardgame.games.GameEvaluator;
+import com.openclassrooms.cardgame.games.HighCardGameEvaluator;
 import com.openclassrooms.cardgame.model.Deck;
 import com.openclassrooms.cardgame.view.View;
 
 public class Games {
 	
 	public static void main(String args[]) {
-		GameController gc = new GameController(new Deck(), new View(), new GameEvaluator());
+		GameController gc = new GameController(new Deck(), new View(), new HighCardGameEvaluator());
+		//GameController gc = new GameController(new Deck(), new View(), new LowCardGameEvaluator());
 		gc.run();
 	}
 }
---
+++ OC-Java-SolidMvc-CardGame/src/com/openclassrooms/cardgame/games/LowCardGameEvaluator.java
@@ -0,0 +1,46 @@
+package com.openclassrooms.cardgame.games;
+
+import java.util.List;
+
+import com.openclassrooms.cardgame.model.Player;
+import com.openclassrooms.cardgame.model.PlayingCard;
+
+public class LowCardGameEvaluator implements GameEvaluator {
+
+	@Override
+	public Player evaluateWinner(List<Player> players) {
+		Player bestPlayer = null;
+		int bestRank = -1;
+		int bestSuit = -1;
+
+		for (Player player : players) {
+			boolean newBestPlayer = false;
+
+			if (bestPlayer == null) {
+				newBestPlayer = true;
+			} else {
+				PlayingCard pc = player.getCard(0);
+				int thisRank = pc.getRank().value();
+				if (thisRank <= bestRank) {
+					if (thisRank < bestRank) {
+						newBestPlayer = true;
+					} else {
+						if (pc.getSuit().value() < bestSuit) {
+							newBestPlayer = true;
+						}
+					}
+				}
+			}
+
+			if (newBestPlayer) {
+				bestPlayer = player;
+				PlayingCard pc = player.getCard(0);
+				bestRank = pc.getRank().value();
+				bestSuit = pc.getSuit().value();
+			}
+		}
+
+		return bestPlayer;
+	}
+
+}
